238858dfb26062a3e294f2adb6640ed88b8c80b3
龚欣 <87526845@qq.com>
2015-06-19 21:55:20 +0800

    Initial Commit

---
+++ 瀑布流/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  瀑布流
+//
+//  Created by 龚欣 on 15/6/19.
+//  Copyright (c) 2015年 gong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
